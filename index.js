4a1628940804f2dd0e9e74f8e5f45bf3ae3922b0
Emrebj <152190598+Emrebj@users.noreply.github.com>
2024-07-27 17:01:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -204,3 +204,17 @@ process.on("warning", (warn) => {
 process.on("unhandledRejection", (reason, promise) => {
     console.log(reason);
 });
+
+// Sunucu oluşturma ve proje aktivitesi sağlama.
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Web sunucu
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} numaralı bağlantı noktasında yürütülüyor.`);
+});
